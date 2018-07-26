2cf1b4f6285cfd2e69e3ff17b4e45379b84913de
Lucien Najev <luciennajev@hotmail.com>
2018-07-26 16:48:02 +0100

    added the write to a csv file method

--- directory.rb
+++ directory.rb
@@ -89,6 +89,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -105,12 +106,28 @@ def process(selection)
     students = input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit #THIS WILL CAUSE THE PROGRAM TO TERMINATE
   else
     puts "I don't know what you mean, try again"
   end
 end
+
+
+def save_students
+  # Open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 
 
 def interactive_menu
