2f441925536c4ffdec0051a8890d6e3922ac94f9
Lucien Najev <luciennajev@hotmail.com>
2018-07-26 10:56:52 +0100

    modified the footer, caling the header method inside the print students method, and added a condition to the input_students method to make sure it doesnt count blank names as additional students

--- directory.rb
+++ directory.rb
@@ -12,7 +12,7 @@ def input_students
       cohort = gets.chomp.downcase
     end
 
-    if months.include?(cohort)
+    if months.include?(cohort) && !name.empty?
       students << { name: name, cohort: cohort.to_sym, hobbies: ["murder", "table tennis"], country_of_birth: :america, height: 178 }
       puts students.length > 1 ? "Now we have #{students.count} students" : "Now we have #{students.count} student"
     else
@@ -36,46 +36,53 @@ end
 
 def print_students(students)
 
-  puts "Filter for students whos names start with a specific letter?"
-  puts "Select a letter to filter by or just hit return to skip"
-  letter = gets .chomp
 
-  puts "Filter for students by the length of their name?"
-  puts "select a number of characters as the max length of the name or just return to print all names"
-  max_chars = gets.chomp.to_i
+  if students.length != 0
+    puts "Filter for students whos names start with a specific letter?"
+    puts "Select a letter to filter by or just hit return to skip"
+    letter = gets.delete_suffix("\n") #HAVE REPLACED CHOMP HERE WITH Delete_suffix("\n")
 
+    puts "Filter for students by the length of their name?"
+    puts "select a number of characters as the max length of the name or just return to print all names"
+    max_chars = gets.chomp.to_i
 
-  x = 0
-  cohort_list(students).each do |cohort|
-    students.each_with_index do |student, i|
-      result = "#{( x + 1 )}: #{students[i][:name]} (#{students[i][:cohort]} cohort)"
-      if letter.empty? && max_chars == 0 && students[i][:cohort] == cohort
-        puts result.center(50)
-        x += 1
-      elsif letter.empty? && max_chars != 0 && students[i][:cohort] == cohort
-        puts result.center(50) if students[i][:name].size < (max_chars)
-        x += 1
-      elsif !letter.empty? && max_chars == 0 && students[i][:cohort] == cohort
-        puts result.center(50) if students[i][:name].start_with?(letter)
-        x += 1
-      elsif students[i][:cohort] == cohort
-        puts result.center(50) if students[i][:name].start_with?(letter) && students[i][:name].size < (max_chars - 1)
-        x += 1
+    print_header #Calling the header here , it looks a bit better in the terminal
+
+    x = 0
+    cohort_list(students).each do |cohort|
+      students.each_with_index do |student, i|
+        result = "#{( x + 1 )}: #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+        if letter.empty? && max_chars == 0 && students[i][:cohort] == cohort
+          puts result.center(50)
+          x += 1
+        elsif letter.empty? && max_chars != 0 && students[i][:cohort] == cohort
+          puts result.center(50) if students[i][:name].size < (max_chars)
+          x += 1
+        elsif !letter.empty? && max_chars == 0 && students[i][:cohort] == cohort
+          puts result.center(50) if students[i][:name].start_with?(letter)
+          x += 1
+        elsif students[i][:cohort] == cohort
+          puts result.center(50) if students[i][:name].start_with?(letter) && students[i][:name].size < (max_chars - 1)
+          x += 1
+        end
       end
     end
   end
+
 end
 
 
 
 
 def print_footer(names)
+  puts
   puts "Overall we have #{names.count} great students".center(50)
+  puts
 end
 
 
 students = input_students
-print_header
+#print_header
 print_students(students)
 print_footer(students)
 
