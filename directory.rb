8fe7f01ff6987ffbf2ec9a3b7d0c747e0bf4ac4f
Lucien Najev <luciennajev@hotmail.com>
2018-07-26 15:46:26 +0100

    Interactive menu method split up into smaller methods

--- directory.rb
+++ directory.rb
@@ -1,8 +1,7 @@
+@students = [] #An empty array accessible to all methods
+
 def input_students
-
-  students = []
   months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
-
   while true do
     puts "Please enter the name of the student or type quit to exit"
     name = gets.chomp
@@ -13,27 +12,22 @@ def input_students
     end
 
     if months.include?(cohort) && !name.empty?
-      students << { name: name, cohort: cohort.to_sym, hobbies: ["murder", "table tennis"], country_of_birth: :america, height: "6 foot 2 inches" }
-      puts students.length > 1 ? "Now we have #{students.count} students" : "Now we have #{students.count} student"
+      @students << { name: name, cohort: cohort.to_sym, hobbies: ["murder", "table tennis"], country_of_birth: :america, height: "6 foot 2 inches" }
+      puts @students.length > 1 ? "Now we have #{@students.count} students" : "Now we have #{@students.count} student"
     else
       puts "sorry i didnt understand...."
     end
   end
-  students
 end
 
 
-def max_width_names(students)
-  columns = students.map { |details| details.values }.transpose
+def max_width_names
+  columns = @students.map { |details| details.values }.transpose
   widths = columns.map do |column|
     column.max_by { |word| word.length}.length
   end
   widths[0]
 end
-
-
-
-
 
 
 def print_header
@@ -42,15 +36,14 @@ def print_header
 end
 
 
-def cohort_list(students)
-  students.map { |student| student[:cohort] }.uniq
+def cohort_list
+  @students.map { |student| student[:cohort] }.uniq
 end
 
 
+def print_students
 
-def print_students(students)
-
-  if students.length != 0
+  if @students.length != 0
     puts "Filter for students whos names start with a specific letter?"
     puts "Select a letter to filter by or just hit return to skip"
     letter = gets.delete_suffix("\n") #HAVE REPLACED CHOMP HERE WITH Delete_suffix("\n")
@@ -62,20 +55,20 @@ def print_students(students)
     print_header #Calling the header here , it looks a bit better in the terminal
 
     x = 0
-    cohort_list(students).each do |cohort|
-      students.each_with_index do |student, i|
-        result = "#{( x + 1 )}: #{students[i][:name]}#{' '*((max_width_names(students) + 5) - (students[i][:name]).length)}(#{students[i][:cohort]} cohort)"
-        if letter.empty? && max_chars == 0 && students[i][:cohort] == cohort
-          puts result#.center(50)
+    cohort_list.each do |cohort|
+      @students.each_with_index do |student, i|
+        result = "#{( x + 1 )}: #{@students[i][:name]}#{' '*((max_width_names + 5) - (@students[i][:name]).length)}(#{@students[i][:cohort]} cohort)"
+        if letter.empty? && max_chars == 0 && @students[i][:cohort] == cohort
+          puts result
           x += 1
-        elsif letter.empty? && max_chars != 0 && students[i][:cohort] == cohort
-          puts result if students[i][:name].size < (max_chars)
+        elsif letter.empty? && max_chars != 0 && @students[i][:cohort] == cohort
+          puts result if @students[i][:name].size < (max_chars)
           x += 1
-        elsif !letter.empty? && max_chars == 0 && students[i][:cohort] == cohort
-          puts result if students[i][:name].start_with?(letter)
+        elsif !letter.empty? && max_chars == 0 && @students[i][:cohort] == cohort
+          puts result if @students[i][:name].start_with?(letter)
           x += 1
-        elsif students[i][:cohort] == cohort
-          puts result if students[i][:name].start_with?(letter) && students[i][:name].size < (max_chars - 1)
+        elsif @students[i][:cohort] == cohort
+          puts result if @students[i][:name].start_with?(letter) && @students[i][:name].size < (max_chars - 1)
           x += 1
         end
       end
@@ -86,49 +79,46 @@ def print_students(students)
 end
 
 
-def print_footer(names)
+def print_footer
   puts
-  puts "Overall we have #{names.count} great students".center(50)
+  puts "Overall we have #{@students.count} great students".center(50)
   puts
 end
 
-#************************************************************************************
 
-#puts "Welcome to the Student Directory"
-#students = input_students
-#print_students(students)
-#print_footer(students)
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
 
+def show_students
+  print_students
+  print_footer
+end
 
-def interactive_menu
-  students = []
-  loop do
-  #1. Print menu to the screen asking user what they would like to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-  #2. read the input and save it to a variable
-    selection = gets.chomp
-  #3. do what the user has asked
-    case selection
-    when "1"
-      #INPUT THE STUDENTS
-      students = input_students
-    when "2"
-      #SHOW THE STUDENTS
-      print_students(students)
-      print_footer(students)
-    when "9"
-      exit #THIS WILL CAUSE THE PROGRAM TO TERMINATE
-    else
-      puts "I don't know what you mean, try again"
-    end
 
-  #4. repeat from step 1
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit #THIS WILL CAUSE THE PROGRAM TO TERMINATE
+  else
+    puts "I don't know what you mean, try again"
   end
 end
 
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
 
 interactive_menu
 
